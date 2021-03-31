5c03212689e057ddd7f74524bb39736b907a8826
amulya gowda <amulyams92@gmail.com>
2021-03-31 12:52:31 +0530

    first commit

---
+++ frontend/src/data.js
@@ -0,0 +1,87 @@
+const data = {
+
+    products: [
+        
+        {
+            _id: '1',
+            name: 'casual knee length frock',
+            category: 'casuals',
+            image: '/images/dress1.jpg',
+            price: 700,
+            brand: 'Vanheusen',
+            rating:4.5,
+            numOfReviews: 10,
+            description:'comfortable and elegant casual wear for party'
+            
+        },
+
+        {
+            _id: '2',
+            name: 'casual knee length frock',
+            category: 'casuals',
+            image: '/images/dress2.jpg',
+            price: 900,
+            brand: 'Vanheusen',
+            rating:4.5,
+            numOfReviews: 12,
+            description:'comfortable and elegant casual wear for party'
+            
+        },
+
+        {
+            _id: '3',
+            name: 'casual knee length frock',
+            category: 'casuals',
+            image: '/images/dress3.jpg',
+            price: 750,
+            brand: 'Vanheusen',
+            rating:4.3,
+            numOfReviews: 8,
+            description:'comfortable and elegant casual wear for party'
+            
+        },
+
+        {
+            _id: '4',
+            name: 'casual knee length frock',
+            category: 'casuals',
+            image: '/images/dress4.jpg',
+            price: 1200,
+            brand: 'Vanheusen',
+            rating:4.5,
+            numOfReviews: 30,
+            description:'comfortable and elegant casual wear for party'
+            
+        },
+
+        {
+            _id: '5',
+            name: 'casual knee length frock',
+            category: 'casuals',
+            image: '/images/dress5.jpg',
+            price: 890,
+            brand: 'Vanheusen',
+            rating:3.9,
+            numOfReviews: 5,
+            description:'comfortable and elegant casual wear for party'
+            
+        },
+
+        {
+            _id: '6',
+            name: 'casual knee length frock',
+            category: 'casuals',
+            image: '/images/dress6.jpg',
+            price: 650,
+            brand: 'Vanheusen',
+            rating:4.5,
+            numOfReviews: 15,
+            description:'comfortable and elegant casual wear for party'
+            
+        },
+
+
+    ]
+}
+
+export default data;
